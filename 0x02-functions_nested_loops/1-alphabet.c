f4a14d199e22ca230de9312653aec30bb663331e
Marksman5404 <akinyemido@gmail.com>
2022-10-19 06:17:12 -0700

    alphabeth

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,25 +1,26 @@
 #include "main.h"
-  
+
 /**
  * main - Entry point
  *
+ *
  * Return: Always 0 (Success)
+ *
  */
-void print_alphabet(void)
 
+void print_alphabet(void)
 {
 
 int n;
 
-for (n = 'a'; n <='z'; n++)
+for (n = 'a'; n <= 'z'; n++)
 
 {
 
 _putchar(n);
+
 }
 
 _putchar('\n');
- 
+
 }
-
-
