7e50d35b209e9026d19e31aba47a887d8830345a
Marksman5404 <akinyemido@gmail.com>
2022-10-19 07:06:28 -0700

    alphabeth

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,8 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- *
- * Return: Always 0 (Success)
- *
- */
+ * print_alphabet - print all alphabelt in lowercase
+*/
 
 void print_alphabet(void)
 {
