5271850e6e45e6ebf9ac49b16e877eb894a41876
Marksman5404 <akinyemido@gmail.com>
2022-10-20 04:42:58 -0700

    Write a function that checks for a digit (0 through 9).

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_isdigit - Write a function that checks for alphabetic character.
+ * @c: checkmate c
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
+ */
+int _isdigit(int c)
+{
+if (c >= '1' && c <= '9')
+
+return (1);
+
+else
+
+return (0);
+
+}
